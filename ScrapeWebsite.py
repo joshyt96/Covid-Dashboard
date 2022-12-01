66ba73006ac66647ce775ced7b8bb860afd080b6
tjohnson1416 <johnson.trevor.kent@gmail.com>
2022-12-01 14:57:51 -0700

    Removed lines

--- ScrapeWebsite.py
+++ ScrapeWebsite.py
@@ -1,17 +1,6 @@
 import requests
 from bs4 import BeautifulSoup 
-
-sauce = requests.get('https://www.worldometers.info/coronavirus/#countries')
-soup = BeautifulSoup(sauce.text, 'html.parser')
-tables = soup.find_all('table')
-with open('csvfile.csv','w') as csv:
-    for row in tables[0].find_all('tr')[8:239]:
-        line = ""
-        for td in row.find_all(['td', 'th']):
-            line += '"' + td.text + '",'
-        csv.write(line + '\n')
 import json
-from bs4 import BeautifulSoup
 import pandas as pd
 
 # Create an URL object, used to make a page object
